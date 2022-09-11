d128ef3a234279295f1e21d2ac59557378baadcd
Rohan1279 <muniruddinrohan@gmail.com>
2022-09-11 18:51:50 +0600

    basic calculation done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
 const digitsContainer = document.getElementById("digits-container");
-const operatorsContainer = document.getElementById('operators-container')
+const operatorsContainer = document.getElementById("operators-container");
 const outputField = document.getElementById("output-field");
+
+let leftOperand = '0';
+let rightOperand = '0';
+let operator = "";
 digitsContainer.addEventListener("click", (e) => {
   if (e.target.localName === "button") {
-    outputField.value = e.target.innerText;
+    outputField.innerText += e.target.innerText;
+    if (operator === "") {
+      leftOperand += parseInt(e.target.innerText);
+      console.log(leftOperand);
+    } else {
+      rightOperand += parseInt(e.target.innerText);
+      console.log(rightOperand);
+    }
   }
 });
-operatorsContainer.addEventListener('click',(e)=>{
-    if (e.target.localName === "button") {
-        outputField.value = e.target.innerText;
+operatorsContainer.addEventListener("click", (e) => {
+    operator = '';
+  if (e.target.localName === "button") {
+    outputField.innerText += e.target.innerText;
+    operator = e.target.innerText;
+    console.log(operator);
+  }
+});
+const equalsButton = document.getElementById('btn-equals').addEventListener('click',()=>{
+    if (operator === 'X') {
+        const result  =  parseInt(leftOperand)*parseInt(rightOperand);
+        outputField.innerText = result;
     }
-    
+    else if(operator === '+'){
+        const result = parseInt(leftOperand)+parseInt(rightOperand);
+        outputField.innerText = result;
+    }
+    else{
+        const result =  parseInt(leftOperand)-parseInt(rightOperand);
+        outputField.innerText = result;
+    }
 })
+
