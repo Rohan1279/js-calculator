f9fbbeee305f873ab8f80bdd0c2aa85a511746d5
Rohan1279 <muniruddinrohan@gmail.com>
2022-09-11 19:23:07 +0600

    button element added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 const digitsContainer = document.getElementById("digits-container");
+
+for (let i = 1; i < 10; i++) {
+   const button = document.createElement('button')
+   button.classList.add('bg-zinc-300', 'text-xl', 'w-10', 'h-10', 'text-center', 'p-2',  'my-2', 'rounded-full', 'mx-auto')
+   button.innerText = i;
+   digitsContainer.appendChild(button);
+} 
+
 const operatorsContainer = document.getElementById("operators-container");
 const outputField = document.getElementById("output-field");
 
@@ -6,6 +14,7 @@ let leftOperand = '0';
 let rightOperand = '0';
 let operator = "";
 digitsContainer.addEventListener("click", (e) => {
+  
   if (e.target.localName === "button") {
     outputField.innerText += e.target.innerText;
     if (operator === "") {
