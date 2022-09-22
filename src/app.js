80d68a8a420b39725f7352e8f2ddf42f828efce4
Rohan1279 <muniruddinrohan@gmail.com>
2022-09-22 12:48:25 +0600

    clear button added

--- src/app.js
+++ src/app.js
@@ -1,33 +1,42 @@
 const digitsContainer = document.getElementById("digits-container");
 
 for (let i = 1; i < 10; i++) {
-   const button = document.createElement('button')
-   button.classList.add('bg-zinc-300', 'text-xl', 'w-10', 'h-10', 'text-center', 'p-2',  'my-2', 'rounded-full', 'mx-auto')
-   button.innerText = i;
-   digitsContainer.appendChild(button);
-} 
+  const button = document.createElement("button");
+  button.classList.add(
+    "bg-zinc-300",
+    "text-xl",
+    "w-10",
+    "h-10",
+    "text-center",
+    "p-2",
+    "my-2",
+    "rounded-full",
+    "mx-auto"
+  );
+  button.innerText = i;
+  digitsContainer.appendChild(button);
+}
 
 const operatorsContainer = document.getElementById("operators-container");
 const outputField = document.getElementById("output-field");
 
-let leftOperand = '0';
-let rightOperand = '0';
+let leftOperand = "0";
+let rightOperand = "0";
 let operator = "";
 digitsContainer.addEventListener("click", (e) => {
-  
   if (e.target.localName === "button") {
     outputField.innerText += e.target.innerText;
     if (operator === "") {
       leftOperand += parseInt(e.target.innerText);
-      console.log(leftOperand);
+      // console.log(leftOperand);
     } else {
       rightOperand += parseInt(e.target.innerText);
-      console.log(rightOperand);
+      // console.log(rightOperand);
     }
   }
 });
 operatorsContainer.addEventListener("click", (e) => {
-    operator = '';
+  operator = "";
   if (e.target.localName === "button") {
     outputField.innerText += e.target.innerText;
     operator = e.target.innerText;
@@ -35,22 +44,28 @@ operatorsContainer.addEventListener("click", (e) => {
   }
 });
 
-
-
-
-const equalsButton = document.getElementById('btn-equals').addEventListener('click',(e)=>{
-    e.stopImmediatePropagation()
-    if (operator === 'X') {
-        const result  =  parseInt(leftOperand)*parseInt(rightOperand);
-        outputField.innerText = result;
+const equalsButton = document
+  .getElementById("btn-equals")
+  .addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+    if (operator === "X") {
+      const result = parseInt(leftOperand) * parseInt(rightOperand);
+      outputField.innerText = result;
+    } else if (operator === "+") {
+      const result = parseInt(leftOperand) + parseInt(rightOperand);
+      outputField.innerText = result;
+    } else {
+      const result = parseInt(leftOperand) - parseInt(rightOperand);
+      outputField.innerText = result;
     }
-    else if(operator === '+'){
-        const result = parseInt(leftOperand)+parseInt(rightOperand);
-        outputField.innerText = result;
-    }
-    else{
-        const result =  parseInt(leftOperand)-parseInt(rightOperand);
-        outputField.innerText = result;
-    }
-})
-
+  });
+const clearButton = document
+  .getElementById("btn-clear")
+  .addEventListener("click", () => {
+    console.log("clear!!!");
+    outputField.innerText = "";
+    leftOperand = "0";
+    rightOperand = "0";
+    operator = "";
+    // console.log(outputField.innerText);
+  });
